6d6b12746f0aa5adbdf2a8b3f0cb63405fa2f744
JohnDaise <jdaise@gmail.com>
2023-07-06 17:11:56 -0400

    formatting and additionnal padding

--- ui.py
+++ ui.py
@@ -13,27 +13,37 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-
-
         # Score Label
-        score_label = Label(text="Score: ", padx=20, pady=20, bg=THEME_COLOR, fg=WHITE, font=(FONT_NAME, 20))
-        score_label.grid(column=1, row=0)
+        self.score_label = Label(text="Score: ", padx=20, pady=20, bg=THEME_COLOR, fg=WHITE, font=(FONT_NAME, 20))
+        self.score_label.grid(column=1, row=0)
 
-        # Question Text Window
-        canvas = Canvas(width=300, height=250, bg=WHITE, highlightthickness=0)
-        question_text = canvas.create_text(150, 125, width=275, text="This is what a sample question will look like. Of course "
-                                                          "this is a", fill="black", font=(FONT_NAME, 20, "italic"))
-        canvas.grid(column=0, row=1, columnspan=2)
+        # Canvas
+        self.canvas = Canvas(width=300, height=250, bg=WHITE, highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=275, text="This is what a sample question will look like. Of course "
+                                                          "this is a",
+            fill="black",
+            font=(FONT_NAME, 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         # Buttons
         check_image = PhotoImage(file="./images/true.png")
         x_image = PhotoImage(file="./images/false.png")
 
-        true_button = Button(image=check_image, padx=20, pady=20, highlightthickness=0, command=self.select_true)
-        true_button.grid(column=0, row=2)
+        self.true_button = Button(image=check_image,
+                                  padx=20, pady=20,
+                                  highlightthickness=0,
+                                  command=self.select_true)
+        self.true_button.grid(column=0, row=2)
 
-        false_button = Button(image=x_image, padx=20, pady=20, highlightthickness=0, command=self.select_false)
-        false_button.grid(column=1, row=2)
+        self.false_button = Button(image=x_image,
+                                   padx=20,
+                                   pady=20,
+                                   highlightthickness=0,
+                                   command=self.select_false)
+        self.false_button.grid(column=1, row=2)
 
         self.window.mainloop()
 
